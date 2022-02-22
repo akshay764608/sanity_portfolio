dcf1924fb922412a28811250430f44da35dae3a4
unknown <akshay764608@gmail.com>
2022-02-22 19:30:05 +0530

    Upto skills and experience

---
+++ src/wrapper/MotionWrap.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const MotionWrap = (Component, classNames) =>
+  function HOC() {
+    return (
+      <div>
+        <motion.div
+          whileInView={{ opacity: [0, 0, 1], y: [100, 50, 0] }}
+          transition={{ duration: 0.5 }}
+          className={`${classNames} app__flex`}
+        >
+          <Component />
+        </motion.div>
+      </div>
+    );
+  };
+
+export default MotionWrap;
